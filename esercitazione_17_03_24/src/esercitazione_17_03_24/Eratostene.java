2e5e06507c2de2867357e458281b8fc97d397180
Angelo Gargantini <angelo.gargantini@unibg.it>
2017-03-24 12:41:10 +0100

    aggiunto prog

---
+++ esercitazione_17_03_24/src/esercitazione_17_03_24/Eratostene.java
@@ -0,0 +1,9 @@
+package esercitazione_17_03_24;
+
+import prog.io.ConsoleInputManager;
+
+public class Eratostene {
+public static void main(String[] args) {
+	ConsoleInputManager in = new ConsoleInputManager();
+}
+}
