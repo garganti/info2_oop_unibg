e069597a808c2f9cfe639fc7a91352515ca908d3
Angelo Gargantini <angelo.gargantini@unibg.it>
2017-05-30 09:00:50 +0200

    cap 11 e 13

--- esercitazione_17_04_26/src/te_gestoreeventi/Evento.java
+++ esercitazione_17_04_26/src/te_gestoreeventi/Evento.java
@@ -3,11 +3,21 @@ package te_gestoreeventi;
 import prog.utili.Data;
 import prog.utili.Sequenza;
 
-public class Evento {
+public class Evento implements Comparable<Evento>{
 
 	private String nome;
-	Data inizio, fine;
-	Sequenza<Sala> sale;
+	private Data inizio, fine;
+	private Sequenza<Sala> sale;
+	
+	public Evento(){
+		
+	}
+	
+	// ordina rispetto la data di inizio
+	@Override
+	public int compareTo(Evento o) {
+		return inizio.compareTo(o.fine);
+	}
 	
 	
 }
--- esercitazione_17_04_26/src/te_gestoreeventi/GestoreEventi.java
+++ esercitazione_17_04_26/src/te_gestoreeventi/GestoreEventi.java
@@ -1,5 +1,13 @@
 package te_gestoreeventi;
 
+import prog.utili.SequenzaOrdinata;
+
 public class GestoreEventi {
+
+	SequenzaOrdinata<Evento> eventi;
+
+	public void inserisciEvento(Evento e) {
+
+	}
 
 }
