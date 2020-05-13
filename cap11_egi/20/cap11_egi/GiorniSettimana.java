65bb7c4d22e39fc439c53c73c04c59158d9f95ea
Angelo Gargantini <angelo.gargantini@unibg.it>
2020-05-13 14:00:25 +0200

    lez

---
+++ cap11_egi/20/cap11_egi/GiorniSettimana.java
@@ -0,0 +1,41 @@
+package cap11_egi;
+
+import java.util.Arrays;
+
+public enum GiorniSettimana {
+	
+	LUN(8),MAR(7),MER(10),GIO(15),VEN(8),SAB(4),DOM(0);
+
+	// campo 
+	private int oreLavorative;
+
+	// costruttore 
+	GiorniSettimana(int o) {oreLavorative = o;}
+	
+	int numeroOreLavorative() {
+		// if this == LUN
+		return oreLavorative;
+	}
+	
+	
+	void foo() {}
+	
+	public static void main(String[] args) {
+		GiorniSettimana g = LUN;
+		
+		System.out.println(g.toString());		
+		// 1. name
+		System.out.println(g.name());
+		// 2. ordinal
+		System.out.println(g.ordinal());
+		// metodi statici
+		System.out.println(Arrays.toString(GiorniSettimana.values()));
+		// comodo da intero a enum
+		int dom = 6;
+		System.out.println(GiorniSettimana.values()[dom]);
+		//GiorniSettimana superdomenica = new GiorniSettimana();
+		
+	}
+	
+	
+}
