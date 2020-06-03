7af82a8552228dd4d16ed661f7b36e1a07fbe67e
Angelo Gargantini <angelo.gargantini@unibg.it>
2020-06-03 14:25:16 +0200

    coda

--- cap13_strutturedati/20/cap13_strutturedati/Coda.java
+++ cap13_strutturedati/20/cap13_strutturedati/Coda.java
@@ -24,12 +24,22 @@ public class Coda<E> {
 		ultimo = temp;		
 	}
 	
+	E preleva() {
+		// se coda è vuota lancia eccezione
+		if (primo==null) throw new RuntimeException("coda vuota");
+		E temp = primo.dato;
+		primo = primo.pros;
+		// la coda adesso è vuota?
+		if (primo == null) ultimo = null;
+		return temp;
+	}
+	
 	@Override
 	public String toString() {
-		String  s = "";
+		String  s = "[";
 		for(Nodo i = primo; i!= null; i = i.pros)
 			s += i.dato + " ";
-		return s;
+		return s+ "]";
 	}
 	
 	public static void main(String[] args) {
@@ -38,6 +48,16 @@ public class Coda<E> {
 		nomi.aggiungi("aaaa");
 		nomi.aggiungi("bbbb");
 		System.out.println(nomi.toString());
+		System.out.println(nomi.preleva());
+		System.out.println(nomi.toString());
+		System.out.println(nomi.preleva());
+		System.out.println(nomi.toString());
+		System.out.println(nomi.preleva());
+		System.out.println(nomi.toString());
+		// attenzione la coda è vuota
+		System.out.println(nomi.preleva());
+
+		
 	}
 	
 	
