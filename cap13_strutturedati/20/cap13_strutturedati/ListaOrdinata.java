a812d19af6559ba2a9a019a4654ac2ba7eda055f
Angelo Gargantini <angelo.gargantini@unibg.it>
2020-06-03 15:43:03 +0200

    rimuovi

--- cap13_strutturedati/20/cap13_strutturedati/ListaOrdinata.java
+++ cap13_strutturedati/20/cap13_strutturedati/ListaOrdinata.java
@@ -22,6 +22,7 @@ public class ListaOrdinata<E extends Comparable<E>> {
 	public void inserisci(E e) {
 		NodoLista p = inizio;
 		NodoLista q = null;
+		// for(;p != null && p.dato.compareTo(e) < 0; q = p, p = p.pros);
 		while (p != null && p.dato.compareTo(e) < 0) {
 			q = p;
 			p = p.pros;
@@ -38,7 +39,30 @@ public class ListaOrdinata<E extends Comparable<E>> {
 		}
 	}
 
-
+	void cancella(E e) {
+		NodoLista p = inizio;
+		NodoLista q = null;
+//		while(p!=null && p.dato.compareTo(e)!=0) {
+		while(p!=null && p.dato.compareTo(e)<0) {
+			q = p;
+			p = p.pros;
+		}
+		// sono uscito senza trovarlo
+		if (p == null || p.dato.compareTo(e) > 0) {
+			System.out.print("elemento "+e +" non trovato" );
+			if (p==null) System.out.println("sono arrivato in fondo");
+			else System.out.println(" ho trovato " + p.dato + " che è > di " +e);
+		} else {
+			// lo rimuovo
+			if (q != null) {
+				q.pros = p.pros;
+			} else {
+				// primo elemento
+				inizio = p.pros;
+			}
+		}
+	}	
+	
 	@Override
 	public String toString() {
 		String result = "[";
@@ -59,6 +83,16 @@ public class ListaOrdinata<E extends Comparable<E>> {
 		numeri.inserisci(5); // in mezzo
 		System.out.println(numeri);
 		numeri.inserisci(1); // in mezzo
+		System.out.println(numeri);
+		// cancella
+		numeri.cancella(5); // in mezzo
+		System.out.println(numeri);
+		numeri.cancella(300); // in mezzo
+		System.out.println(numeri);
+		// primo elemento
+		numeri.cancella(1); // in mezzo
+		System.out.println(numeri);
+		numeri.cancella(7); // in mezzo
 		System.out.println(numeri);
 	}
 }
