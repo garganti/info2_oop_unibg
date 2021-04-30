82965116dfb4626fc8a609169c8cb16c63a7fbe6
Angelo Gargantini <angelo.gargantini@unibg.it>
2021-04-30 10:31:40 +0200

    ora

---
+++ esercizicompleti/21/orario/OraHM.java
@@ -0,0 +1,25 @@
+package orario;
+
+public class OraHM extends Ora{
+	
+	int ora;
+	int minuti;
+	
+	public OraHM(int h, int m) {
+		if (h<0 || h > 23 || m < 0 || m> 59)
+			throw new RuntimeException("ora non valida");
+		ora = h; minuti = m;
+	}
+	@Override
+	public int getHH() {return ora;}
+
+	@Override
+	public int getMM() {return minuti;}
+	
+	@Override
+	public String toString() {
+		//return "OraHM["+ora + ":" + minuti + "]";
+		return String.format("OraHM[%2d:%2d",ora, minuti);
+	}
+
+}
---
+++ esercizicompleti/21/orario/ProvaOrari.java
@@ -0,0 +1,8 @@
+package orario;
+
+public class ProvaOrari {
+
+	public static void main(String[] args) {
+		Ora now = new OraHM(10, 30);
+	}
+}
